4d6c52cdbe99e48fc5c227878dfd0df90da0f221
Dulzz <dusanz91@hotmail.com>
2017-07-26 01:39:09 +0200

    Add files via upload

--- js/sticky.js
+++ js/sticky.js
@@ -1,1 +1,59 @@
+  /* sticky */
 
+	 $(document).ready(function(){
+
+	 	$('.js-header').waypoint(function(direction){
+	 		if(direction == "down"){
+	 			$('nav').addClass('sticky');
+	 		}else{
+	 			$('nav').removeClass('sticky');
+	 		}
+	 	}, {
+	 		offset: '10%;'
+	 	});
+	 	/*
+	 	var waypoints = $('#handler-first').waypoint(function(direction) {
+	  		notify(this.element.id + ' hit 25% from top of window') 
+		}, {
+	  	offset: '25%'
+		})
+		*/
+
+  /* */
+
+  /* scroll to the top */
+
+  	
+
+		$("#top").click(function() {
+		  $("html, body").animate({ scrollTop: 0 }, "slow");
+		  return false;
+		});
+
+	
+
+  /* */ 
+
+
+  /* scroll on navigation */ 
+
+  	$(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+
+
+  /* */
+
+  });
